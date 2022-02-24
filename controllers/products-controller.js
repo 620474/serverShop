9a9a17279a9994a43c0f605516e7e9e38c0c17fe
Maksim Sychev <maksim_sychev@epam.com>
2022-02-24 16:14:27 +0300

    change return type product controller

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -5,7 +5,7 @@ module.exports = {
     index: function (req,res) {
         Product.find({})
             .then((products)=>{
-                res.status(200).json({products: products})
+                res.status(200).json(products)
             })
             .catch((err) => console.log(err));
     },
